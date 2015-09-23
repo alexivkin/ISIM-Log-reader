2fafc99a9cd6bd4226da6220496da8940e9623c0
Alex <alexivkin@users.noreply.github.com>
2015-09-23 09:33:04 -0700

    New filters

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ Get python 2.7 and run:
 
 `python digest_itim_log.py <trace.log|msg.log>`
 
-The output will be saved with a "_digest.csv" suffix
+The output will be saved with a "_digest.csv" suffix to the same folder as the source. This script also works under jython 2.7 and jython standalone 2.7, but may need more heap than default because of the extensive regexps.
 
-You can refine the digest further by filtering out standard events
+You can refine the digest further by filtering out standard events.
 
 `digest_filter.bat trace_digest.log trace_important.log`
+
+(you need Cygwin grep for this to work)
--- digest_filter.bat
+++ digest_filter.bat
@@ -1,4 +1,4 @@
 @echo off
-rem A list of filters to remove all not important/successful events from a log digest
-rem run with input and output file names
-grep -v -E ",.?(adding|Empty|requestID|using global|using Orphan|.*OU=Hidden|.*SUCCESSFUL|Failed first char|Failed numeric test|Processed data collected|Refreshing schema|###|.*successful|0 disallowed|SvcID=)" %1 > %2
+rem An example list of filters to remove all not important/successful events from a log digest
+rem Run with input and output file names
+grep -v -E ",.?(ProfileTable refresh time|entry|Log statements for PMR|Entry|Date,Time|Refreshing|adding|Empty|requestID .*? completed successfully|message is outbound|Session ID from SOAP Message|Session id.*set in the message context|using global|using Orphan|.*OU=Hidden|.*=SUCCESSFUL|Processed data collected|Refreshing schema|###|.* successful|0 disallowed|SvcID=|session ID in Handler on the return path)" %1 > %2
